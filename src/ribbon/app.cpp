a88088801386ece1c26bd59706e9d0e4112971f4
tepumasuta <thinkingocean2@gmail.com>
2023-02-18 22:17:47 +0300

    Made `App` a `Retranslator` of `WindowClosedEvent` & Implemented window closing logic

--- src/ribbon/app.cpp
+++ src/ribbon/app.cpp
@@ -1,20 +1,33 @@
 #include "app.hpp"
 
 #include <GLFW/glfw3.h>
+
 namespace Ribbon
 {
     App::App()
     {
         m_Window = std::unique_ptr<Window>(Window::Create());
+
+        m_WindowClosedSubscriptionPtr = m_Window->GetWindowClosedEvent().Subscribe(*this);
     }
     
-    App::~App() {}
-    
+    App::~App()
+    {
+        m_Window->GetWindowClosedEvent().Unsubscribe(m_WindowClosedSubscriptionPtr);
+    }
+
     void App::Run()
     {
         while (m_Running)
         {
             m_Window->OnUpdate();
         }
+    }
+
+    void App::OnEvent(Events::WindowClosedEvent& e)
+    {
+        m_Running = false;
+        Notify(&e);
+        e.Handled = true;
     }
 } // namespace Ribbon
--- src/ribbon/app.hpp
+++ src/ribbon/app.hpp
@@ -1,21 +1,23 @@
 #pragma once
 #include "common.hpp"
 #include "window.hpp"
+#include "events/app_event.hpp"
 
 namespace Ribbon
 {
-    class RIB_API App
+    class RIB_API App : Events::Retranslator<Events::WindowClosedEvent>
     {
     public:
         App();
         virtual ~App();
         void Run();
+        void OnEvent(Events::WindowClosedEvent& e) override;
     private:
         std::unique_ptr<Window> m_Window;
         bool m_Running = true;
+        Listener<Events::WindowClosedEvent>* m_WindowClosedSubscriptionPtr;
     };
 
     // Should be defined in CLIENT
     App* CreateApp();
 } // namespace Ribbon
-
