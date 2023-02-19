3d27770a4eacd8d3caf0302e4688d3d714530b2b
tepumasuta <thinkingocean2@gmail.com>
2023-02-19 15:05:08 +0300

    Made `App`'s `OnEvent` private

--- src/ribbon/app.hpp
+++ src/ribbon/app.hpp
@@ -2,6 +2,7 @@
 #include "common.hpp"
 #include "window.hpp"
 #include "events/app_event.hpp"
+#include "layers/layer_stack.hpp"
 
 namespace Ribbon
 {
@@ -11,10 +12,12 @@ namespace Ribbon
         App();
         virtual ~App();
         void Run();
+    private:
         void OnEvent(Events::WindowClosedEvent& e) override;
     private:
         std::unique_ptr<Window> m_Window;
         bool m_Running = true;
+        // ListenerPointers
         Events::WindowClosedEvent::ListenerPointer m_WindowClosedSubscriptionPtr;
     };
 
